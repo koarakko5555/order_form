ce488f065c247d9d93fd5a9cdec919d468d28540
koarakko5555 <hosayaan.1016@gmail.com>
2025-02-16 15:57:19 +0900

    ラジオボタンを追加

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -17,6 +17,8 @@ RSpec.describe '注文フォーム', type: :system do
     fill_in 'お届け先住所', with: delivery_address
     select '銀行振込', from: '支払い方法'
     fill_in 'その他・ご要望', with: other_comment
+    choose '配信を希望する'
+
     click_on '確認画面へ'
 
     expect(current_path).to eq confirm_orders_path
@@ -35,7 +37,8 @@ RSpec.describe '注文フォーム', type: :system do
     expect(order.telephone).to eq telephone
     expect(order.delivery_address).to eq delivery_address
     expect(order.payment_method_id).to eq 2
-    expect(order.other_comment).to eq 2
+    expect(order.other_comment).to eq other_comment
+    expect(order.direct_mail_enabled).to eq true
 
   end
 
@@ -49,6 +52,7 @@ RSpec.describe '注文フォーム', type: :system do
       fill_in 'お届け先住所', with: delivery_address
       select '銀行振込', from: '支払い方法'
       fill_in 'その他・ご要望', with: other_comment
+      choose '配信を希望する'
 
       click_on '確認画面へ'
   
@@ -66,6 +70,7 @@ RSpec.describe '注文フォーム', type: :system do
         fill_in 'お届け先住所', with: delivery_address
         select '銀行振込', from: '支払い方法'
         fill_in 'その他・ご要望', with: other_comment 
+        choose '配信を希望する'
     
         click_on '確認画面へ'
   
@@ -81,6 +86,8 @@ RSpec.describe '注文フォーム', type: :system do
         expect(page).to have_field '電話番号', with: telephone
         expect(page).to have_field 'お届け先住所', with: delivery_address
         expect(page).to have_select '支払い方法', selected: '銀行振込'
+        expect(page).to have_field 'その他・ご要望', with: other_comment
+        expect(page).to have_checked_field '配信を希望する'
 
         click_on '確認画面へ'
         
@@ -99,6 +106,8 @@ RSpec.describe '注文フォーム', type: :system do
         expect(order.telephone).to eq telephone
         expect(order.delivery_address).to eq delivery_address
         expect(order.payment_method_id).to eq 2
+        expect(order.other_comment).to eq other_comment
+        expect(order.direct_mail_enabled).to eq true
       end
     end
   end
