fa259d56fe317feb04382bea0099541fc94c988e
koarakko5555 <hosayaan.1016@gmail.com>
2025-02-24 15:18:55 +0900

    商品の注文項目のテストを追加

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe '注文フォーム', type: :system do
     fill_in '電話番号', with: telephone
     fill_in 'お届け先住所', with: delivery_address
     select '銀行振込', from: '支払い方法'
+    select 'たのしいオレンジ(200円/個)', from: '商品'
+    fill_in '数量', with: 3
+
     fill_in 'その他・ご要望', with: other_comment
     choose '配信を希望する'
     check 'm-1グランプリ'
@@ -43,6 +46,10 @@ RSpec.describe '注文フォーム', type: :system do
     expect(order.direct_mail_enabled).to eq true
     expect(order.inflow_source_ids).to eq [2, 3]
 
+    expect(order.order_products.size).to eq 1
+    expect(order.order_products[0].product_id).to eq 2
+    expect(order.order_products[0].quantity).to eq 3
+
   end
 
   context '入力情報に不備がある場合' do
@@ -54,6 +61,8 @@ RSpec.describe '注文フォーム', type: :system do
       fill_in '電話番号', with: '090123456789'
       fill_in 'お届け先住所', with: delivery_address
       select '銀行振込', from: '支払い方法'
+      select 'たのしいオレンジ(200円/個)', from: '商品'
+      fill_in '数量', with: 3
       fill_in 'その他・ご要望', with: other_comment
       choose '配信を希望する'
       check 'm-1グランプリ'
@@ -65,7 +74,7 @@ RSpec.describe '注文フォーム', type: :system do
       expect(page).to have_content '電話番号は11文字以内で入力してください'
     end
 
-    context '画面で戻るを推した場合' do
+    context '画面で戻るを押した場合' do
       it '商品を注文できること' do
         visit new_order_path
     
@@ -74,6 +83,8 @@ RSpec.describe '注文フォーム', type: :system do
         fill_in '電話番号', with: telephone
         fill_in 'お届け先住所', with: delivery_address
         select '銀行振込', from: '支払い方法'
+        select 'たのしいオレンジ(200円/個)', from: '商品'
+        fill_in '数量', with: 3
         fill_in 'その他・ご要望', with: other_comment 
         choose '配信を希望する'
         check 'm-1グランプリ'
@@ -93,6 +104,10 @@ RSpec.describe '注文フォーム', type: :system do
         expect(page).to have_field '電話番号', with: telephone
         expect(page).to have_field 'お届け先住所', with: delivery_address
         expect(page).to have_select '支払い方法', selected: '銀行振込'
+
+        expect(page).to have_select '商品', selected: 'たのしいオレンジ(200円/個)'
+        expect(page).to have_field '数量', with: 3
+
         expect(page).to have_field 'その他・ご要望', with: other_comment
         expect(page).to have_checked_field '配信を希望する'
 
@@ -122,8 +137,117 @@ RSpec.describe '注文フォーム', type: :system do
         expect(order.other_comment).to eq other_comment
         expect(order.direct_mail_enabled).to eq true
         expect(order.inflow_source_ids).to eq [2, 3]
+        expect(order.order_products[0].product_id).to eq 2
+        expect(order.order_products[0].quantity).to eq 3
 
       end
+    end
+
+    context '商品を追加して注文した場合' do
+      it '商品を注文できること' do
+        visit new_order_path
+    
+        fill_in 'お名前', with: name
+        fill_in 'メールアドレス', with: email
+        fill_in '電話番号', with: telephone
+        fill_in 'お届け先住所', with: delivery_address
+        select '銀行振込', from: '支払い方法'
+        select 'たのしいオレンジ(200円/個)', from: '商品'
+        fill_in '数量', with: 3
+
+        click_on '商品を追加する'
+        
+        select '不思議なマンゴー(1,200円/個)', from: 'order[order_products_attributes][1][product_id]'
+        fill_in 'order[order_products_attributes][1][quantity]', with: 4
+
+        fill_in 'その他・ご要望', with: other_comment
+        choose '配信を希望する'
+        check 'm-1グランプリ'
+        check 'r-1グランプリ'
+    
+        click_on '確認画面へ'
+    
+        expect(current_path).to eq confirm_orders_path
+    
+        click_on 'OK'
+        expect(current_path).to eq complete_orders_path
+        expect(page).to have_content "#{name}様"
+    
+        # 完了ページを再訪するとトップページに遷移する
+        visit complete_orders_path
+        expect(current_path).to eq new_order_path
+    
+        order = Order.last
+        expect(order.name).to eq name
+        expect(order.email).to eq email
+        expect(order.telephone).to eq telephone
+        expect(order.delivery_address).to eq delivery_address
+        expect(order.payment_method_id).to eq 2
+        expect(order.other_comment).to eq other_comment
+        expect(order.direct_mail_enabled).to eq true
+        expect(order.inflow_source_ids).to eq [2, 3]
+    
+        expect(order.order_products.size).to eq 2
+        expect(order.order_products[0].product_id).to eq 2
+        expect(order.order_products[0].quantity).to eq 3
+        expect(order.order_products[1].product_id).to eq 6
+        expect(order.order_products[1].quantity).to eq 4
+    
+      end    
+    end
+
+    context '商品を追加して、削除してから注文した場合' do
+      it '商品を注文できること' do
+        visit new_order_path
+    
+        fill_in 'お名前', with: name
+        fill_in 'メールアドレス', with: email
+        fill_in '電話番号', with: telephone
+        fill_in 'お届け先住所', with: delivery_address
+        select '銀行振込', from: '支払い方法'
+
+        select 'たのしいオレンジ(200円/個)', from: '商品'
+        fill_in '数量', with: 3
+
+        click_on '商品を追加する'
+        
+        select '不思議なマンゴー(1,200円/個)', from: 'order[order_products_attributes][1][product_id]'
+        fill_in 'order[order_products_attributes][1][quantity]', with: 4
+
+        click_on '削除', match: :first 
+
+        fill_in 'その他・ご要望', with: other_comment
+        choose '配信を希望する'
+        check 'm-1グランプリ'
+        check 'r-1グランプリ'
+    
+        click_on '確認画面へ'
+    
+        expect(current_path).to eq confirm_orders_path
+    
+        click_on 'OK'
+        expect(current_path).to eq complete_orders_path
+        expect(page).to have_content "#{name}様"
+    
+        # 完了ページを再訪するとトップページに遷移する
+        visit complete_orders_path
+        expect(current_path).to eq new_order_path
+    
+        order = Order.last
+        expect(order.name).to eq name
+        expect(order.email).to eq email
+        expect(order.telephone).to eq telephone
+        expect(order.delivery_address).to eq delivery_address
+        expect(order.payment_method_id).to eq 2
+        expect(order.other_comment).to eq other_comment
+        expect(order.direct_mail_enabled).to eq true
+        expect(order.inflow_source_ids).to eq [2, 3]
+    
+        expect(order.order_products.size).to eq 1
+        expect(order.order_products[0].product_id).to eq 6
+        expect(order.order_products[0].quantity).to eq 4
+    
+      end    
     end
   end
 end
